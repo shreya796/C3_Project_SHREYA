16fbe5beaf3791d08b7ea398c1cf06ba1000de65
shreya796 <shreya07.gupta@gmail.com>
2020-12-14 20:25:03 +0530

    Part 3: Failing test case

--- Restaurant_project -Student version/src/test/java/RestaurantTest.java
+++ Restaurant_project -Student version/src/test/java/RestaurantTest.java
@@ -79,4 +79,26 @@ class RestaurantTest {
                 ()->restaurant.removeFromMenu("French fries"));
     }
     //<<<<<<<<<<<<<<<<<<<<<<<MENU>>>>>>>>>>>>>>>>>>>>>>>>>>>>>
+
+    @Test
+    public void adding_item_to_menu_should_display_total_price(){
+        restaurant.addToMenu(name "sweet corn soup",price 119);
+        restaurant.addToMenu(name "vegetable lasgane",price 269);
+        List<Item> menu = restaurant.getMenu();
+        int actualTotalPrice = restaurant.showTotalPrice(menu);
+        assertEquals(totalPrice,actualTotalPrice);
+    }
+    @Test
+    public void adding_no_item_to_menu_should_display_total_price_as_0(){
+        List<Item> menu = restaurant.getMenu();
+        int totalPrice = 0;
+        int actualTotalPrice =restaurant.showTotalPrice(menu);
+        assertEquals(totalPrice,actualTotalPrice);
+    }
+    @Test
+    public void removing_an_item_from_menu_should_display_updated_price() throws itemNotFoundException {
+        restaurant.addToMenu(name "sweet corn soup",price 119);
+        restaurant.addToMenu(name "vegetable lasgane",price 269);
+        List<Item>menu = restaurant.getMenu();
+    }
 }
